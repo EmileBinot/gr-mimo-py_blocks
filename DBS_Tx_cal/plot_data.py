ca4a83603c6ab24f0ac1e95ebfd2160491acd1e9
EmileBinot <emile.binot2@gmail.com>
2022-07-26 16:39:39 +0200

    new data

--- DBS_Tx_cal/plot_data.py
+++ DBS_Tx_cal/plot_data.py
@@ -9,9 +9,8 @@ dirname = os.path.dirname(__file__)
 rcParams['axes.xmargin'] = 0
 rcParams['axes.ymargin'] = 0.05
 
-# data_cal = np.genfromtxt(os.path.join(dirname, "data_21_07_2022/data.csv"), delimiter=',')
-data_cal = np.genfromtxt(os.path.join(dirname, "data_22_07_2022/data.csv"), delimiter=',')
-data_no_cal = np.genfromtxt(os.path.join(dirname, "data_21_07_2022/data_no_cal.csv"), delimiter=',')
+data_cal = np.genfromtxt(os.path.join(dirname, "data/data_26_07_2022/data.csv"), delimiter=',')
+data_no_cal = np.genfromtxt(os.path.join(dirname, "data/data_26_07_2022/data_no_cal.csv"), delimiter=',')
 
 angles_cal = data_cal[:,2]
 angles_no_cal = data_no_cal[:,2]
@@ -28,16 +27,16 @@ if np.shape(data_cal)[1] == 4 :
 
 # Recalculate angles from [-90°, 90°] to [0°,180°]
 for i in range(len(angles_cal)):
-    if angles_cal[i] > 0 :
+    if angles_cal[i] > 90 :
+        angles_cal[i] = angles_cal[i]-180
+    else :
         angles_cal[i] = angles_cal[i]
-    else :
-        angles_cal[i] = angles_cal[i] + 180
 
-for i in range(len(angles_no_cal)):
-    if angles_no_cal[i] > 0 :
-        angles_no_cal[i] = angles_no_cal[i]
-    else :
-        angles_no_cal[i] = angles_no_cal[i] + 180
+# for i in range(len(angles_no_cal)):
+#     if angles_no_cal[i] > 0 :
+#         angles_no_cal[i] = angles_no_cal[i]
+#     else :
+#         angles_no_cal[i] = angles_no_cal[i] + 180
 
 # Statistical properties :
 mean_cal = np.round(np.mean(angles_cal), 1)
@@ -48,7 +47,7 @@ std_cal = np.round(angles_cal.std(), 1)
 print(f"mean max. power angle : {mean_cal}°")
 
 # FIG #1
-bins = np.linspace(0, 180,180)
+bins = np.linspace(-90, 90,180)
 kwargs = dict(alpha=0.6, bins = bins)
 
 fig = plt.figure()
@@ -58,7 +57,7 @@ plt.axvline(mean_cal, color="dodgerblue", linestyle='dashed', label=f"Mean calib
 plt.axvline(mean_cal-std_cal, color="dodgerblue", linestyle='dotted', label=f"Std. dev. calibrated= {std_cal}°")
 plt.axvline(mean_cal+std_cal, color="dodgerblue", linestyle='dotted')
 plt.legend(loc='upper right');plt.xlabel("Angles (°)");plt.ylabel("Occurences")
-fig.suptitle("Angles of Maximum power (data)", fontsize=12)
+fig.suptitle(f"Angles of Maximum power ({len(angles_cal)} trys)", fontsize=12)
 
 # FIG #2
 bins = np.linspace(-180,180,180)
@@ -73,7 +72,7 @@ ax[1].hist(Tx_PLL_phase_cal, **kwargs, color="red", label="Tx PLLs phase shift")
 ax[1].legend(loc='upper right')
 ax[1].set_xlabel("Phase shift (°)")
 ax[1].set_ylabel("Occurences")
-fig.suptitle("PLLs phase differences (data)", fontsize=12)
+fig.suptitle(f"PLLs phase differences ({len(angles_cal)} trys)", fontsize=12)
 
 # # FIG #3
 # fig, ax = plt.subplots(2, sharex=True)
